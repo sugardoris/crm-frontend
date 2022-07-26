6b1e21652906a576cdfc6861cd574a789a23ae72
Doris Šugar <sugardoris1@yahoo.com>
2022-07-26 12:09:46 +0200

    Subscriber landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -11,6 +12,10 @@ import {MatToolbarModule} from "@angular/material/toolbar";
 import { SubscriberTableComponent } from './subscriber/subscriber-table/subscriber-table.component';
 import {MatTableModule} from "@angular/material/table";
 import { SubscriberLandingPageComponent } from './subscriber/subscriber-landing-page/subscriber-landing-page.component';
+import { SubscriberSearchComponent } from './subscriber/subscriber-search/subscriber-search.component';
+import {MatInputModule} from "@angular/material/input";
+import {MatAutocompleteModule} from "@angular/material/autocomplete";
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
@@ -18,16 +23,21 @@ import { SubscriberLandingPageComponent } from './subscriber/subscriber-landing-
     SidenavContentComponent,
     SubscriberTableComponent,
     SubscriberLandingPageComponent,
+    SubscriberSearchComponent,
   ],
-    imports: [
-        BrowserModule,
-        BrowserAnimationsModule,
-        MatSidenavModule,
-        MatListModule,
-        MatIconModule,
-        MatToolbarModule,
-        MatTableModule
-    ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatTableModule,
+    MatInputModule,
+    MatAutocompleteModule,
+    MatButtonModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })

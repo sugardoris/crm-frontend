daba986ada6ea0d31b1c8c81934bb95ab2ba26cf
Doris Šugar <sugardoris1@yahoo.com>
2022-07-29 14:37:05 +0200

    Add routing module, fix city styles

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import {NgModule} from "@angular/core";
+import {RouterModule, Routes} from "@angular/router";
+import {SubscriberLandingPageComponent} from "./subscriber/subscriber-landing-page/subscriber-landing-page.component";
+import {SubscriberDetailsPageComponent} from "./subscriber/subscriber-details-page/subscriber-details-page.component";
+import {CityLandingPageComponent} from "./city/city-landing-page/city-landing-page.component";
+import {
+  PublicationsLandingPageComponent
+} from "./publication/publications-landing-page/publications-landing-page.component";
+import {
+  SubscriptionTypesLandingComponent
+} from "./subscription_type/subscription-types-landing/subscription-types-landing.component";
+import {UserLandingPageComponent} from "./user/user-landing-page/user-landing-page.component";
+
+const routes: Routes = [
+  {path: 'subscribers', component: SubscriberLandingPageComponent},
+  {path: 'subscribers/:id', component: SubscriberDetailsPageComponent},
+  {path: 'cities', component: CityLandingPageComponent},
+  {path: 'publications', component: PublicationsLandingPageComponent},
+  {path: 'subscription-types', component: SubscriptionTypesLandingComponent},
+  {path: 'users', component: UserLandingPageComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
